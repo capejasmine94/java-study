91a6e7a5e76918959d8e199818cac3b1c91e53b5
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-09-19 18:58:41 +0900

    test

---
+++ src/intermediatePart1/lang/string/CharArrayMain.java
@@ -0,0 +1,13 @@
+package intermediatePart1.lang.string;
+
+public class CharArrayMain {
+
+    public static void main(String[] args) {
+
+        char[] charArr = new char[]{'a', 'b', 'c', 'd', 'e', 'f'};
+        System.out.println(charArr);
+
+        String str = "hello";
+        System.out.println(str);
+    }
+}
