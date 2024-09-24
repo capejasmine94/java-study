dec96171fa4f569c850222dabbdd8b5400a052b8
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-09-24 13:41:27 +0900

    래퍼 클래스 기본형

---
+++ src/intermediatePart1/lang/wrapper/MyIntegerMethodMain1.java
@@ -0,0 +1,19 @@
+package intermediatePart1.lang.wrapper;
+
+public class MyIntegerMethodMain1 {
+    public static void main(String[] args) {
+
+        MyInteger myInteger = new MyInteger(10);
+
+        int value = myInteger.getValue();
+        System.out.println("value = " + value);
+
+        int i1 = myInteger.compareTo(5);
+        int i2 = myInteger.compareTo(10);
+        int i3 = myInteger.compareTo(20);
+
+        System.out.println("i1 = " + i1);
+        System.out.println("i2 = " + i2);
+        System.out.println("i3 = " + i3);
+    }
+}
---
+++ src/intermediatePart1/lang/wrapper/WrapperClassMain.java
@@ -0,0 +1,14 @@
+package intermediatePart1.lang.wrapper;
+
+public class WrapperClassMain {
+
+    public static void main(String[] args) {
+
+        Integer newInteger = new Integer(10);
+        Integer integerObj = Integer.valueOf(10);
+        System.out.println("newInteger = " + newInteger);
+        System.out.println("integerObj = " + integerObj);
+
+
+    }
+}
