4b6829b99db5624e57c8284723c40a0ecd062c0a
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-09-24 13:56:50 +0900

    래퍼클래스 valueOf()
    래퍼클래스에 맞는 범위 값 은 재사용 반환을 한다. 그 외에 숫자는 객체를 새로 만든다.

--- src/intermediatePart1/lang/wrapper/WrapperClassMain.java
+++ src/intermediatePart1/lang/wrapper/WrapperClassMain.java
@@ -5,10 +5,28 @@ public class WrapperClassMain {
     public static void main(String[] args) {
 
         Integer newInteger = new Integer(10);
-        Integer integerObj = Integer.valueOf(10);
+        Integer integerObj = Integer.valueOf(10); // -128 ~ 127 자주 사용하는 숫자 값 재사용 반환 , 그 외에 숫자는 객체를 새로 만들다.
+        Integer integerObj1 = Integer.valueOf(10);
+
+        Long longObj = Long.valueOf(100);
+        Double doubleObj = Double.valueOf(10.5);
+
         System.out.println("newInteger = " + newInteger);
         System.out.println("integerObj = " + integerObj);
+        System.out.println("longObj = " + longObj);
+        System.out.println("doubleObj = " + doubleObj);
 
+        System.out.println("내부 값 읽기");
+        int intValue = integerObj.intValue();
+        System.out.println("intValue = " + intValue);
+        long longValue = longObj.longValue();
+        System.out.println("longValue = " + longValue);
 
+        System.out.println("비교");
+        System.out.println("== : " + (newInteger == integerObj));
+        System.out.println("equals : " + newInteger.equals(integerObj));
+
+        System.out.println("== : " + (integerObj == integerObj1));
+        System.out.println("equals : " + newInteger.equals(integerObj));
     }
 }
