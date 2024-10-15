e17ec98bc1613d882fb0666a6934689844fc4531
capejasmine94 <123536114+capejasmine94@users.noreply.github.com>
2024-10-15 18:05:45 +0900

    enum 기본

---
+++ src/intermediatePart1/enumration/ex1/StringGrade.java
@@ -0,0 +1,10 @@
+package intermediatePart1.enumration.ex1;
+
+public class StringGrade {
+
+    public static final String BASIC = "BASIC";
+    public static final String GOLD = "GOLD";
+    public static final String DIAMOND = "DIAMOND";
+
+
+}
